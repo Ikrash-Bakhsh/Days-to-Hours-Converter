f6149c85ead49e99b447a2cb5b59a94c6b233d9d
Ikrash Bakhsh <153372614+Ikrash-Bakhsh@users.noreply.github.com>
2024-01-13 21:47:34 +0500

    Add files via upload

---
+++ Task 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the days to convert them into hours = ";
+	cin>>a;
+	b=a*24;
+	cout<<"Days in hours = "<<b;
+	return 0;
+}
